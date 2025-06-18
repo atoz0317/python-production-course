015a35aef40caea7dcb25c20389ef6f4f404efe8
atoz0317 <atoz0317.programming@gmail.com>
2025-06-18 16:13:44 +0800

    test: add pytest fixtures

--- tests/test__generate_project.py
+++ tests/test__generate_project.py
@@ -1,14 +1,11 @@
-import json
 import shutil
-import subprocess
-from copy import deepcopy
+import sys
 from pathlib import Path
 from typing import Iterator
 
 import pytest
 
-THIS_DIR = Path(__file__).parent
-PROJECT_DIR = (THIS_DIR / "../").resolve()
+from tests.utils.project import generate_project
 
 
 @pytest.fixture
@@ -17,30 +14,6 @@ def project_dir() -> Iterator[Path]:
     generated_repo_dir = generate_project(template_values)
     yield generated_repo_dir
     shutil.rmtree(path=generated_repo_dir)
-
-
-def generate_project(template_values: dict[str, str]):
-    template_values = deepcopy(template_values)
-    cookiecutter_config = {"default_context": template_values}
-    cookiecutter_config_fpath = PROJECT_DIR / "tests/cookiecutter.json"
-
-    cookiecutter_config_fpath.write_text((json.dumps(cookiecutter_config)))
-
-    cmd = [
-        "cookiecutter",
-        str(PROJECT_DIR),
-        "--output-dir",
-        str(PROJECT_DIR / "sample"),
-        "--no-input",
-        "--config-file",
-        str(cookiecutter_config_fpath),
-    ]
-    print("COMMAND: ", " ".join(cmd))
-    subprocess.run(cmd, check=True)
-
-    generated_repo_dir = PROJECT_DIR / "sample" / template_values["repo_name"]
-
-    return generated_repo_dir
 
 
 def test__can_generate_project(project_dir: Path):
